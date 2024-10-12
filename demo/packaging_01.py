0683f1e2eeedacb0315a834d1ae72ba65222ccbb
Sonia Parmar <soniaparmar24@gmail.com>
2024-10-13 00:03:14 +0200

    remove breakpoint

--- demo/packaging_01.py
+++ demo/packaging_01.py
@@ -2,7 +2,5 @@ from animal_shelter.data import load_data
 from animal_shelter.features import add_features
 
 animal_outcomes = load_data("data/test.csv")
-breakpoint()
 with_features = add_features(animal_outcomes)
-breakpoint()
 print(with_features.head())
