7e9c5dfbcc2c2537d2cbf9faa6bfcc06e047f65c
Sonia Parmar <soniaparmar24@gmail.com>
2024-10-13 00:10:23 +0200

    add fixture for two test functions

--- tests/test_features.py
+++ tests/test_features.py
@@ -1,6 +1,7 @@
 from pandas.testing import assert_series_equal
 import pandas as pd
 from animal_shelter import features
+import pytest
 
 def test_check_is_dog():
     is_dog = pd.Series(["Dog", "Cat",])
@@ -14,18 +15,6 @@ def test_check_has_name():
     expected = pd.Series([True, True, False])
     assert_series_equal(actual, expected)
 
-def test_get_sex():
-    sex = pd.Series(["Male", "Female", "unknown"])
-    actual = features.get_sex(sex)
-    expected = pd.Series(["male", "female", "unknown"])
-    assert_series_equal(actual, expected)
-
-def test_get_neutered():
-    neutered = pd.Series(["neutered", "spayed", "intact"])
-    actual = features.get_neutered(neutered)
-    expected = pd.Series(["fixed", "fixed", "intact"])
-    assert_series_equal(actual, expected)
-
 def test_get_hair_type():
     hair_types = pd.Series(["shorthair", "medium hair", "longhair", "short", "curly"])
     actual = features.get_hair_type(hair_types)
@@ -33,3 +22,18 @@ def test_get_hair_type():
     assert_series_equal(actual, expected)
 
 # def test_compute_days_upon_outcome():
+
+@pytest.fixture()
+def fixture_get_sex_neutered():
+    sex_upon_outcome = pd.Series(["spayed Female", "spayed Male", "neutered Female", "neutered Male", "intact Female", "intact Male"])
+    return sex_upon_outcome
+
+def test_get_sex(fixture_get_sex_neutered):
+    actual = features.get_sex(fixture_get_sex_neutered)
+    expected = pd.Series(["female", "male", "female", "male", "female", "male"])
+    assert_series_equal(actual, expected)
+
+def test_get_neutered(fixture_get_sex_neutered):
+    actual = features.get_neutered(fixture_get_sex_neutered)
+    expected = pd.Series(["fixed", "fixed", "fixed", "fixed","intact", "intact"])
+    assert_series_equal(actual, expected)
